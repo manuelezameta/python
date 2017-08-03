af780c5b252294a5426bda4980f9da208201f979
Manuel Lezameta <manuelezameta@gmail.com>
2017-08-03 00:22:09 -0500

    add new lines

--- HelloWorld/learnPython.py
+++ HelloWorld/learnPython.py
@@ -423,24 +423,27 @@ ship_row = random_row(board)
 ship_col = random_col(board)
 
 for turn in range(4):
-    print("Turn", turn + 1)
-
     guess_row = int(input("Guess Row: "))
     guess_col = int(input("Guess Col: "))
 
-    print(ship_col)
-    print(ship_row)
-
     if guess_row == ship_row and guess_col == ship_col:
-        print("Congratulations! You sank my battleship!")
+        print
+        "Congratulations! You sank my battleship!"
+        break
     else:
         if guess_row not in range(5) or \
                         guess_col not in range(5):
-            print("Oops, that's not even in the ocean.")
+            print
+            "Oops, that's not even in the ocean."
         elif board[guess_row][guess_col] == "X":
             print("You guessed that one already.")
         else:
-            print("You missed my battleship!")
+            print
+            "You missed my battleship!"
             board[guess_row][guess_col] = "X"
 
+        if turn == 3:
+            print("Game Over")
+
         print_board(board)
+        print("Turn", turn + 1)   # print turn + 1 here
